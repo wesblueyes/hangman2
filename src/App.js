9dcaede38d3ed6ef11fae11a7900b7aa6f27cdd7
Whitney Smith <whitney@gallerywrappeddesign.com>
2016-08-10 14:24:43 -0400

    Hangman2

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Word from './Word.js'
+import Board from './Board.js';
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      solution: 'HANGMAN',
+      used: [
+      ]
+    }
+  }
+  onPlay = (character) => {
+    this.state.used.push(character)
+    this.setState({
+      used: this.state.used
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+      <img src={'./src/fig7-8.gif'}/>
+      <h1>Count: </h1>
+      <Word solution={this.state.solution} used={this.state.used} />
+        <Board onPlay={this.onPlay}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Board.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import './Board.css'
+
+class Board extends Component {
+  render () {
+    return (
+      <div className='Board'>
+      <h2>Guess the word.</h2>
+      <Alphabet onPlay={this.props.onPlay}/>
+      </div>
+    )
+  }
+}
+class Alphabet extends Component {
+  render () {
+    return (
+      <div className="character-class">
+      <Letter character="A" onPlay={this.props.onPlay}/>
+      <Letter character="B" onPlay={this.props.onPLay}/>
+      <Letter character="C" onPlay={this.props.onPlay}/>
+      <Letter character="D" onPlay={this.props.onPlay}/>
+      <Letter character="E" onPlay={this.props.onPlay}/>
+      <Letter character="F" onPlay={this.props.onPlay}/>
+      <Letter character="G" onPlay={this.props.onPlay}/>
+      <Letter character="H" onPlay={this.props.onPlay}/>
+      <Letter character="I" onPlay={this.props.onPlay}/>
+      <Letter character="J" onPlay={this.props.onPlay}/>
+      <Letter character="K" onPlay={this.props.onPlay}/>
+      <Letter character="L" onPlay={this.props.onPlay}/>
+      <Letter character="M" onPlay={this.props.onPlay}/>
+      <Letter character="N" onPlay={this.props.onPlay}/>
+      <Letter character="O" onPlay={this.props.onPlay}/>
+      <Letter character="P" onPlay={this.props.onPlay}/>
+      <Letter character="Q" onPlay={this.props.onPlay}/>
+      <Letter character="R" onPlay={this.props.onPlay}/>
+      <Letter character="S" onPlay={this.props.onPlay}/>
+      <Letter character="T" onPlay={this.props.onPlay}/>
+      <Letter character="U" onPlay={this.props.onPlay}/>
+      <Letter character="V" onPlay={this.props.onPlay}/>
+      <Letter character="W" onPlay={this.props.onPlay}/>
+      <Letter character="X" onPlay={this.props.onPlay}/>
+      <Letter character="Y" onPlay={this.props.onPlay}/>
+      <Letter character="Z" onPlay={this.props.onPlay}/>
+      </div>
+    )
+  }
+}
+
+class Letter extends Component {
+  handleClick = () => {
+    this.props.onPlay(this.props.character)
+  }
+  render () {
+    return <button onClick={this.handleClick}>{this.props.character}</button>;
+  }
+}
+export default Board
